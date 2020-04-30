ace6fc341a5270e6cd5d57fa896143d352c283bd
rungnapa <rungnapa.itti@gmail.com>
2020-04-30 21:23:04 +0700

    useEffect by settimeup() and use styled-components

--- myapp/src/component/Welcoom.js
+++ myapp/src/component/Welcoom.js
@@ -1,6 +1,27 @@
 import React , {useState , useEffect} from 'react'
-const Welcoom = () => {
+import styled  from 'styled-components'
+
+
+const Box = styled.div`
     
+    color : white;
+    background : pink;
+`
+
+const MessageA = styled.span`
+    background : ${({color}) => color};
+    color : white;
+    font-size : 20px;
+    font-family : 'Fira Code';
+`
+//extend message A 
+const MessageB = styled(MessageA)`
+    font-size : 5px;
+    font-family : Tahoma;
+`
+
+const Welcoom = () => {
+    const [color , setColor] = useState('orange')
     const [message , setMessage] = useState('')
     const [numberA,setNumberA] = useState(0)
     const [numberB,setNumberB] = useState(0)
@@ -27,6 +48,13 @@ const Welcoom = () => {
             <button onClick={() => setNumberB(numberB+1)}>Click Number B</button>
             <br />
             {message ? `Message: ${message}` : 'Loading...'}
+
+            <Box>styled - components</Box>
+
+            <MessageA color={color}>Hello A </MessageA>
+            <MessageB color={color}>Hello B</MessageB>
+            <button style = {{marginTop: 20 , marginLeft : 20}} onClick = {() => setColor('red')}>red</button>
+            <button style = {{marginTop: 20 , marginLeft : 20}} onClick = {() => setColor('yellow')}>yellow</button>
         </div>
 
     )
